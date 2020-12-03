f4ef2fcafa49d130b0af0545f5ecaca616748e8a
magarcd <manuelbadman@gmail.com>
2020-12-03 16:37:22 +0100

    Initial commit

--- style_transfer.py
+++ style_transfer.py
@@ -59,7 +59,7 @@ class Evaluator(object):
     def __init__(self,fetch_loss_and_grads):
         self.loss_value = None
         self.grads_value = None
-		self.fetch_loss_and_grads= fetch_loss_and_grads
+        self.fetch_loss_and_grads= fetch_loss_and_grads
     
     def loss(self,x):
         assert self.loss_value is None
@@ -79,53 +79,52 @@ class Evaluator(object):
         return grad_values
 
 def neural_style_transfer(target_folder_path, style_reference_image_path, output_path):
-
-	# get_ipython().system('wget "https://3.bp.blogspot.com/-gG2TK3WUCeE/WEwqlahXgkI/AAAAAAAADLY/SRCcdZn0yeUKDFrTDGgLaVnRHwjQcAabgCLcB/s1600/mariposa.jpg" -O mariposas.jpg')
+    # get_ipython().system('wget "https://3.bp.blogspot.com/-gG2TK3WUCeE/WEwqlahXgkI/AAAAAAAADLY/SRCcdZn0yeUKDFrTDGgLaVnRHwjQcAabgCLcB/s1600/mariposa.jpg" -O mariposas.jpg')
 	#target_folder_path = 'datasetTransformar'
 	#style_reference_image_path = 'datasetOriginal/100.jpg'
 	target_image = K.variable(preprocess_image(target_image_path))
-	style_reference_image = K.variable(preprocess_image(style_reference_image_path))
-	input_tensor = K.concatenate([target_image,style_reference_image,combination_image],axis=0)
-	model = vgg19.VGG19(input_tensor=input_tensor,weights='imagenet',include_top=False)
-	outputs_dict = dict([(layer.name,layer.output) for layer in model.layers])
-	content_layer = 'block5_conv2'
-	style_layers = ['block1_conv1','block2_conv1','block3_conv1','block4_conv1','block5_conv1']
-	total_variation_weight = 1e-4
-	style_weight = 1.
-	content_weight = 0.025
-	loss = K.variable(0.)
-	layer_features = outputs_dict[content_layer]
-	target_image_features = layer_features[0,:,:,:]
-	combination_features = layer_features[2,:,:,:]
-	loss += content_weight*content_loss(target_image_features,combination_features)
-	for layer_name in style_layers:
-		layer_features = outputs_dict[layer_name]
-		style_reference_features = layer_features[1,:,:,:]
-		combination_features = layer_features[2,:,:,:]
-		sl = style_loss(style_reference_features,combination_features)
-		loss += (style_weight/len(style_layers)) * sl
-	loss += total_variation_weight * total_variation_loss(combination_image)
-	grads = K.gradients(loss,combination_image)[0]
-	fetch_loss_and_grads = K.function([combination_image],[loss,grads])
-	evaluator = Evaluator(fetch_loss_and_grads)
-	# result_prefix = 'my_result'
-	iterations = 100
-	imgs = os.listdir(target_folder_path)
-	for im in [target_folder_path+"/"+img for img in imgs]:
-		x = preprocess_image(im)
-		x = x.flatten()
-		for i in range(iterations):
-			print('Start of iteration',i)
-			start_time = time.time()
-			x,min_val,info = fmin_l_bfgs_b(evaluator.loss,x, fprime=evaluator.grads, maxfun=20)
-			print('Current loss value:', min_val)
-			img = x.copy().reshape((img_height,img_width,3))
-			img = deprocess_image(img)
-			fname = im[im.rfind('/'):]
-			imageio.imwrite(output_path +fname,img)
-			print('Image saved as', fname)
-			end_time = time.time()
-			print('Iteration %d completed in %ds' % (i,end_time - start_time))
+    style_reference_image = K.variable(preprocess_image(style_reference_image_path))
+    input_tensor = K.concatenate([target_image,style_reference_image,combination_image],axis=0)
+    model = vgg19.VGG19(input_tensor=input_tensor,weights='imagenet',include_top=False)
+    outputs_dict = dict([(layer.name,layer.output) for layer in model.layers])
+    content_layer = 'block5_conv2'
+    style_layers = ['block1_conv1','block2_conv1','block3_conv1','block4_conv1','block5_conv1']
+    total_variation_weight = 1e-4
+    style_weight = 1.
+    content_weight = 0.025
+    loss = K.variable(0.)
+    layer_features = outputs_dict[content_layer]
+    target_image_features = layer_features[0,:,:,:]
+    combination_features = layer_features[2,:,:,:]
+    loss += content_weight*content_loss(target_image_features,combination_features)
+    for layer_name in style_layers:
+        layer_features = outputs_dict[layer_name]
+        style_reference_features = layer_features[1,:,:,:]
+        combination_features = layer_features[2,:,:,:]
+        sl = style_loss(style_reference_features,combination_features)
+        loss += (style_weight/len(style_layers)) * sl
+    loss += total_variation_weight * total_variation_loss(combination_image)
+    grads = K.gradients(loss,combination_image)[0]
+    fetch_loss_and_grads = K.function([combination_image],[loss,grads])
+    evaluator = Evaluator(fetch_loss_and_grads)
+    # result_prefix = 'my_result'
+    iterations = 100
+    imgs = os.listdir(target_folder_path)
+    for im in [target_folder_path+"/"+img for img in imgs]:
+        x = preprocess_image(im)
+        x = x.flatten()
+        for i in range(iterations):
+            print('Start of iteration',i)
+            start_time = time.time()
+            x,min_val,info = fmin_l_bfgs_b(evaluator.loss,x, fprime=evaluator.grads, maxfun=20)
+            print('Current loss value:', min_val)
+            img = x.copy().reshape((img_height,img_width,3))
+            img = deprocess_image(img)
+            fname = im[im.rfind('/'):]
+            imageio.imwrite(output_path +fname,img)
+            print('Image saved as', fname)
+            end_time = time.time()
+            print('Iteration %d completed in %ds' % (i,end_time - start_time))
 
 #import matplotlib.pyplot as plt
 #get_ipython().run_line_magic('matplotlib', 'inline')
